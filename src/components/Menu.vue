5135fc776d4f4e901dd345721fc8db7509d06278
Jonathan-trabach <trabachjonathan@gmail.com>
2024-08-12 23:01:34 -0300

    Add Menu Responsivo

---
+++ src/components/Menu.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <button @click="toggleMenu" class="menu-button">
+        ☰
+      </button>
+      <nav :class="{ 'open': isMenuOpen }" class="menu">
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <button @click="toggleMenu" class="close-button">×</button>
+      </nav>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Menu',
+    data() {
+      return {
+        isMenuOpen: false,
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+      },
+    },
+  };
+  </script>
+  
+  <style src="./Menu.css" scoped></style>
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main content area.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+  };
+  </script>
+  
+  <style scoped>
+  .home {
+    text-align: center;
+    padding: 20px;
+  }
+  </style>
+  
