2b6e02acc9f05c8259622fe085a148fa7a404fb3
EdneiTrabach <edneitrabach@hotmail.com>
2024-08-15 14:43:13 -0300

    iclusao icones footer e ajustes no footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,40 +2,40 @@
   <footer class="footer">
     <div class="footer-top">
       <div class="footer-left">
-        <a href="https://goo.gl/maps/crtr-endereco" target="_blank" class="footer-link">
-          <img src="../assets/logos/whatsapp-logo.svg" alt="Endereço" class="footer-icon"/> Endereço do CRTR
+        <a href="https://maps.app.goo.gl/EBn9TgrFhbR47oJD9" target="_blank" class="footer-link">
+          <img src="../assets/logos/localizacao-icon.svg" alt="Endereço" class="footer-icon"/> CRTR - do ES para o World!
         </a>
       </div>
       <div class="footer-center">
-        <a href="https://wa.me/yourwhatsappnumber" target="_blank" class="footer-link">
-          <img src="../assets/logos/whatsapp-logo.svg" alt="Telefone" class="footer-icon"/> +55 11 99999-9999
+        <a href="https://wa.me/27996253020" target="_blank" class="footer-link">
+          <img src="../assets/logos/chamada-telefonica-icon.svg" alt="Telefone" class="footer-icon"/> +55 27 99625-3020
         </a>
       </div>
       <div class="footer-right">
         <a href="https://facebook.com" target="_blank" class="footer-social">
-          <img src="../assets/logos/whatsapp-logo.svg" alt="Facebook" class="footer-social-icon"/>
+          <img src="../assets/logos/facebook-logo.svg" alt="Facebook" class="footer-social-icon"/>
         </a>
         <a href="https://twitter.com" target="_blank" class="footer-social">
-          <img src="../assets/logos/whatsapp-logo.svg" alt="Twitter" class="footer-social-icon"/>
+          <img src="../assets/logos/twitter-logo.svg" alt="Twitter" class="footer-social-icon"/>
         </a>
         <a href="https://instagram.com" target="_blank" class="footer-social">
-          <img src="../assets/logos/whatsapp-logo.svg" alt="Instagram" class="footer-social-icon"/>
+          <img src="../assets/logos/instagram-logo.svg" alt="Instagram" class="footer-social-icon"/>
         </a>
         <a href="https://youtube.com" target="_blank" class="footer-social">
-          <img src="../assets/logos/whatsapp-logo.svg" alt="YouTube" class="footer-social-icon"/>
+          <img src="../assets/logos/whatsapp-logo-footer.svg" alt="YouTube" class="footer-social-icon"/>
         </a>
         <a href="https://spotify.com" target="_blank" class="footer-social">
-          <img src="../assets/logos/whatsapp-logo.svg" alt="Spotify" class="footer-social-icon"/>
+          <img src="../assets/logos/spotify-logo.svg" class="footer-social-icon"/>
         </a>
       </div>
     </div>
 
     <div class="footer-bottom">
       <div class="footer-left">
-        <p>Copyright © 2024. TRABACH SOFTWARES. Todos os direitos reservados.</p>
+        <p>Copyright © 2024. TRABACH SOFTWARES.<br>Todos os direitos reservados.</p>
       </div>
       <div class="footer-right">
-        <p>Desenvolvido por TRABACH SOFTWARES | <a href="tel:+551199999999" class="footer-link">+55 11 99999-9999</a></p>
+        <p>Desenvolvido por TRABACH SOFTWARES<br> <a href="https://wa.me/27996253020" class="footer-link">+55 27 99625-3020</a></p>
       </div>
     </div>
   </footer>
