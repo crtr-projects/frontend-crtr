5059c6760ee9b478803e050533c24dcbff2a9aee
Jonathan-trabach <trabachjonathan@gmail.com>
2024-08-15 12:23:21 -0300

    correcao deploy

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,29 +3,29 @@
     <div class="footer-top">
       <div class="footer-left">
         <a href="https://goo.gl/maps/crtr-endereco" target="_blank" class="footer-link">
-          <img src="../assets/icons/pin.svg" alt="Endereço" class="footer-icon"/> Endereço do CRTR
+          <img src="../assets/logos/whatsapp-logo.svg" alt="Endereço" class="footer-icon"/> Endereço do CRTR
         </a>
       </div>
       <div class="footer-center">
         <a href="https://wa.me/yourwhatsappnumber" target="_blank" class="footer-link">
-          <img src="../assets/icons/phone.svg" alt="Telefone" class="footer-icon"/> +55 11 99999-9999
+          <img src="../assets/logos/whatsapp-logo.svg" alt="Telefone" class="footer-icon"/> +55 11 99999-9999
         </a>
       </div>
       <div class="footer-right">
         <a href="https://facebook.com" target="_blank" class="footer-social">
-          <img src="../assets/icons/facebook.svg" alt="Facebook" class="footer-social-icon"/>
+          <img src="../assets/logos/whatsapp-logo.svg" alt="Facebook" class="footer-social-icon"/>
         </a>
         <a href="https://twitter.com" target="_blank" class="footer-social">
-          <img src="../assets/icons/twitter.svg" alt="Twitter" class="footer-social-icon"/>
+          <img src="../assets/logos/whatsapp-logo.svg" alt="Twitter" class="footer-social-icon"/>
         </a>
         <a href="https://instagram.com" target="_blank" class="footer-social">
-          <img src="../assets/icons/instagram.svg" alt="Instagram" class="footer-social-icon"/>
+          <img src="../assets/logos/whatsapp-logo.svg" alt="Instagram" class="footer-social-icon"/>
         </a>
         <a href="https://youtube.com" target="_blank" class="footer-social">
-          <img src="../assets/icons/youtube.svg" alt="YouTube" class="footer-social-icon"/>
+          <img src="../assets/logos/whatsapp-logo.svg" alt="YouTube" class="footer-social-icon"/>
         </a>
         <a href="https://spotify.com" target="_blank" class="footer-social">
-          <img src="../assets/icons/spotify.svg" alt="Spotify" class="footer-social-icon"/>
+          <img src="../assets/logos/whatsapp-logo.svg" alt="Spotify" class="footer-social-icon"/>
         </a>
       </div>
     </div>
