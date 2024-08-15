7168ae67d092b91ee3926d734477ef6e4a73838e
EdneiTrabach <edneitrabach@hotmail.com>
2024-08-15 16:06:17 -0300

    iclusao imagens parceiros e ajuste footer

--- src/components/ParceirosOficiais.vue
+++ src/components/ParceirosOficiais.vue
@@ -2,7 +2,7 @@
     <div class="parceiros-oficiais">
       <!-- Título centralizado -->
       <h2 class="titulo">Parceiros Oficiais</h2>
-  
+
       <!-- Logos dos parceiros -->
       <div class="parceiros-grid">
         <div class="parceiro-item" v-for="(parceiro, index) in parceiros" :key="index">
@@ -10,14 +10,14 @@
           <p class="parceiro-nome">{{ parceiro.nome }}</p>
         </div>
       </div>
-  
+
       <!-- Logo do WhatsApp no canto direito inferior -->
       <a :href="whatsappLink" class="whatsapp-logo">
         <img src="../assets/logos/whatsapp-logo.svg" alt="WhatsApp" />
       </a>
     </div>
   </template>
-  
+
   <script>
   export default {
     name: 'ParceirosOficiais',
@@ -25,17 +25,16 @@
       return {
         whatsappLink: 'https://wa.me/yourwhatsappnumber', // Link do WhatsApp
         parceiros: [
-          { nome: 'Parceiro 1', logo: 'path/to/parceiro1-logo.jpg' },
-          { nome: 'Parceiro 2', logo: 'path/to/parceiro2-logo.jpg' },
-          { nome: 'Parceiro 3', logo: 'path/to/parceiro3-logo.jpg' },
-          { nome: 'Parceiro 4', logo: 'path/to/parceiro4-logo.jpg' },
-          { nome: 'Parceiro 5', logo: 'path/to/parceiro5-logo.jpg' },
+          { nome: 'BMW', logo: '../' },
+          { nome: 'MERCEDEZ', logo: 'path/to/parceiro2-logo.jpg' },
+          { nome: 'APPLE', logo: 'path/to/parceiro3-logo.jpg' },
+          { nome: 'BEER DAY', logo: 'path/to/parceiro4-logo.jpg' },
+          { nome: 'GOOGLE', logo: 'path/to/parceiro5-logo.jpg' },
           // Adicione mais parceiros conforme necessário
         ],
       };
     },
   };
   </script>
-  
+
   <style src="../components/ParceirosOficiais.css" scoped></style>
-  
