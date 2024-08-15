1879852400d1c30f9d6b2537d9c8046594febc4f
Jonathan-trabach <trabachjonathan@gmail.com>
2024-08-15 18:24:38 -0300

    Adicionando fotos da galeria

--- src/components/Galeria.vue
+++ src/components/Galeria.vue
@@ -1,33 +1,32 @@
 <template>
-    <div class="gallery">
-      <!-- Título da galeria com destaque para a palavra "Fotos" -->
-      <h2 class="gallery-title">
-        Galeria de <span class="highlight">Fotos</span>
-      </h2>
-      <!-- Grid de fotos -->
-      <div class="gallery-grid">
-        <img v-for="(photo, index) in photos" :src="photo" :key="index" class="gallery-photo" alt="Foto da Galeria" />
-      </div>
-      <!-- Botão "ver mais" -->
-      <button class="more-button">Ver mais</button>
+  <div class="gallery">
+    <!-- Título da galeria com destaque para a palavra "Fotos" -->
+    <h2 class="gallery-title">
+      Galeria de <span class="highlight">Fotos</span>
+    </h2>
+    <!-- Grid de fotos -->
+    <div class="gallery-grid">
+      <img v-for="(photo, index) in photos" :src="photo" :key="index" class="gallery-photo" alt="Foto da Galeria" />
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Galeria',
-    data() {
-      return {
-        // Array de URLs de fotos para a galeria
-        photos: [
-          'path/to/photo1.jpg',
-          'path/to/photo2.jpg',
-          'path/to/photo3.jpg'
-        ],
-      };
-    },
-  };
-  </script>
-  
-  <style src="../components/Galeria.css" scoped></style>
-  
+    <!-- Botão "ver mais" -->
+    <button class="more-button">Ver mais</button>
+  </div>
+</template>
+
+<script>
+import image1 from '../assets/galeria/image-1.png';
+import image2 from '../assets/galeria/image-2.png';
+import image3 from '../assets/galeria/image-3.png';
+import image4 from '../assets/galeria/image-4.png';
+
+export default {
+  name: 'Galeria',
+  data() {
+    return {
+      photos: [image1, image2, image3, image4],
+    };
+  },
+};
+</script>
+
+<style src="../components/Galeria.css" scoped></style>
