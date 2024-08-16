d44fbc3d502bbd54dc706f2dc474edc9ecd6566e
Jonathan-trabach <trabachjonathan@gmail.com>
2024-08-16 17:00:10 -0300

    Adicionando lives

---
+++ src/views/LivesView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="lives-view">
+      <div>
+        <Lives /> <!-- Inclui o componente Lives na view -->
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Lives from '../components/Lives.vue'; // Importa o componente Lives
+  
+  export default {
+    name: 'LivesView',
+    components: {
+      Lives, // Registra o componente Lives
+    },
+  };
+  </script>
+  
+  <style src="../views/LivesView.css"></style>
+  
