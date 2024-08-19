50e39d7be5a3f2a956574d95c09d0d3967a95161
EdneiTrabach <edneitrabach@hotmail.com>
2024-08-19 14:11:18 -0300

    atualizacao footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,50 +1,44 @@
 <template>
   <footer class="footer">
-    <div class="footer-top">
-      <div class="footer-left">
-        <a href="https://maps.app.goo.gl/MQbYFzjxTSALuqQr8" target="_blank" class="footer-link">
-          <img src="../assets/logos/localizacao-icon.svg" alt="Endereço" class="footer-icon"/> CRTR - do ES para o World!
-        </a>
-      </div>
-      <div class="footer-center">
+    <div class="footer-content">
+      <div class="footer-contact">
         <a href="https://wa.me/5527997725101" target="_blank" class="footer-link">
           <img src="../assets/logos/chamada-telefonica-icon.svg" alt="Telefone" class="footer-icon"/> +55 27 99772-5101
         </a>
       </div>
-      <div class="footer-right">
-        <a href="https://wa.me/5527997725101" target="_blank" class="footer-social">
+      <div class="footer-social">
+        <a href="https://wa.me/5527997725101" target="_blank" class="footer-social-link">
           <img src="../assets/logos/whatsapp-logo-footer.svg" alt="whatsapp" class="footer-social-icon"/>
         </a>
-        <a href="facebook.com/circuitotrproducoes?mibextid=LQQJ4d" target="_blank" class="footer-social">
+        <a href="facebook.com/circuitotrproducoes?mibextid=LQQJ4d" target="_blank" class="footer-social-link">
           <img src="../assets/logos/facebook-logo.svg" alt="Facebook" class="footer-social-icon"/>
         </a>
-        <a href="https://twitter.com" target="_blank" class="footer-social">
+        <a href="https://twitter.com" target="_blank" class="footer-social-link">
           <img src="../assets/logos/twitter-logo.svg" alt="Twitter" class="footer-social-icon"/>
         </a>
-        <a href="https://www.instagram.com/circuitotrproducoesoficial?utm_source=qr" target="_blank" class="footer-social">
+        <a href="https://www.instagram.com/circuitotrproducoesoficial?utm_source=qr" target="_blank" class="footer-social-link">
           <img src="../assets/logos/instagram-logo.svg" alt="Instagram" class="footer-social-icon"/>
         </a>
-        <a href="https://youtube.com/@circuitotrproducoesoficial" target="_blank" class="footer-social">
+        <a href="https://youtube.com/@circuitotrproducoesoficial" target="_blank" class="footer-social-link">
           <img src="../assets/logos/youtube-logo.svg" alt="YouTube" class="footer-social-icon"/>
         </a>
-        <a href="https://spotify.com" target="_blank" class="footer-social">
+        <a href="https://spotify.com" target="_blank" class="footer-social-link">
           <img src="../assets/logos/spotify-logo.svg" class="footer-social-icon"/>
         </a>
       </div>
+      <div class="footer-location">
+        <a href="https://maps.app.goo.gl/MQbYFzjxTSALuqQr8" target="_blank" class="footer-link">
+          <img src="../assets/logos/localizacao-icon.svg" alt="Endereço" class="footer-icon"/> CRTR - do ES para o World!
+        </a>
+      </div>
     </div>
-
     <div class="footer-bottom">
-      <div class="footer-left">
-        <p>Copyright © 2024. TRABACH SOFTWARES.<br>Todos os direitos reservados.</p>
-      </div>
-      <div class="footer-right">
-        <p>Desenvolvido por TRABACH SOFTWARES<br> <a href="https://wa.me/27996253020" class="footer-link">+55 27 99625-3020</a></p>
-      </div>
+      <p>© 2024 Trabach Softwares <br>Todos os direitos reservados.</p>
     </div>
   </footer>
 </template>
 
-<script>
+<script> 
 export default {
   name: 'Footer',
 };
