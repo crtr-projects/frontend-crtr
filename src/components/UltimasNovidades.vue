b2789a3a4019273a7f8b6b575bee3f41bc1eb624
Jonathan-trabach <trabachjonathan@gmail.com>
2024-08-19 15:41:16 -0300

    Ajuste nas noticias

--- src/components/UltimasNovidades.vue
+++ src/components/UltimasNovidades.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="ultimas-novidades">
+    <div class="section-header">
+      <h1>Últimas Notícias</h1>
+      <p>Confira as últimas atualizações e novidades diretamente aqui!</p>
+    </div>
     <div class="carousel">
       <div class="carousel-container" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
         <div class="carousel-slide" v-for="(newsItem, index) in newsItems" :key="index">
-          <img :src="newsItem.image" :alt="newsItem.title" />
-          <p>{{ newsItem.title }}</p>
+          <div class="slide-content">
+            <img :src="newsItem.image" :alt="newsItem.title" class="slide-image" />
+            <div class="slide-text">
+              <h2>{{ newsItem.title }}</h2>
+              <p>{{ newsItem.description }}</p>
+            </div>
+          </div>
         </div>
       </div>
       <button @click="prevSlide" class="carousel-control prev">&#10094;</button>
@@ -15,6 +24,9 @@
 </template>
 
 <script>
+import news1 from '../assets/noticias/news1.png';
+import news2 from '../assets/noticias/news2.png';
+import news3 from '../assets/noticias/news3.png';
 import '../components/UltimasNovidades.css'; // Importando o CSS separado
 
 export default {
@@ -23,9 +35,9 @@ export default {
     return {
       currentIndex: 0,
       newsItems: [
-        { image: '/assets/noticias/news1.png', title: 'Notícia 1' },
-        { image: '/assets/noticias/news2.png', title: 'Notícia 2' },
-        { image: '/assets/noticias/news3.png', title: 'Notícia 3' },
+        { image: news1, title: 'Notícia 1', description: 'Descrição breve da notícia 1.' },
+        { image: news2, title: 'Notícia 2', description: 'Descrição breve da notícia 2.' },
+        { image: news3, title: 'Notícia 3', description: 'Descrição breve da notícia 3.' },
         // Adicione mais itens conforme necessário
       ],
       interval: null,
@@ -39,7 +51,7 @@ export default {
       this.currentIndex = (this.currentIndex - 1 + this.newsItems.length) % this.newsItems.length;
     },
     startAutoSlide() {
-      this.interval = setInterval(this.nextSlide, 3000); // Muda de slide a cada 3 segundos
+      this.interval = setInterval(this.nextSlide, 5000);
     },
     stopAutoSlide() {
       if (this.interval) {
@@ -55,5 +67,3 @@ export default {
   },
 };
 </script>
-
-<style src="../components/UltimasNovidades.css" scoped></style>
