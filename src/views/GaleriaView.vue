80e2f2a49be079c89150f6c200d243e5921daec3
EdneiTrabach <edneitrabach@hotmail.com>
2024-08-20 12:04:30 -0300

    ajustes galeria - modal

--- src/views/GaleriaView.vue
+++ src/views/GaleriaView.vue
@@ -5,25 +5,44 @@
         Galeria de <span class="highlight">Fotos</span>
       </h2>
       <div class="gallery-grid">
-        <div v-for="(imagem, index) in imagens" :key="index" class="gallery-photo-card">
+        <div v-for="(imagem, index) in imagens" :key="index" class="gallery-photo-card" @click="openImageModal(index)">
           <img :src="imagem.resolvedImage" :alt="imagem.title" class="gallery-photo" />
           <p class="gallery-photo-title">{{ imagem.title }}</p>
           <p class="gallery-photo-description">{{ imagem.description }}</p>
         </div>
       </div>
     </main>
+
+    <!-- Modal de Imagens -->
+    <div v-if="isModalOpen" class="modal-overlay" @click="closeImageModal">
+      <div class="modal-content" @click.stop>
+        <button class="close-button" @click="closeImageModal">X</button>
+        <img :src="currentImage?.resolvedImage" alt="Imagem da galeria" class="modal-image" />
+<!-- Botões de navegação no modal -->
+<button class="navigation-button prev-button" @click="previousImage">&#10094;</button>
+<button class="navigation-button next-button" @click="nextImage">&#10095;</button>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import galeriaData from '../mocks/galeriaMocks.json'; // Importa o arquivo JSON com os dados da galeria
+import galeriaData from '../mocks/galeriaMocks.json';
 
 export default {
   name: 'GaleriaView',
   data() {
     return {
-      imagens: this.resolveImages(galeriaData) // Resolve os caminhos das imagens
+      imagens: this.resolveImages(galeriaData),
+      isModalOpen: false,
+      currentIndex: 0,
     };
+  },
+  computed: {
+    currentImage() {
+      return this.imagens[this.currentIndex];
+    }
   },
   methods: {
     resolveImages(imagens) {
@@ -31,6 +50,19 @@ export default {
         ...imagem,
         resolvedImage: new URL(`../assets/galeria/${imagem.image}`, import.meta.url).href
       }));
+    },
+    openImageModal(index) {
+      this.currentIndex = index;
+      this.isModalOpen = true;
+    },
+    closeImageModal() {
+      this.isModalOpen = false;
+    },
+    prevImage() {
+      this.currentIndex = (this.currentIndex - 1 + this.imagens.length) % this.imagens.length;
+    },
+    nextImage() {
+      this.currentIndex = (this.currentIndex + 1) % this.imagens.length;
     }
   }
 };
